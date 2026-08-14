6d3190af4287f1074d4dc382887a9df8f7b83447
rusirijayodaillesinghe <rusiri@wso2.com>
2026-08-14 14:14:34 +0530

    Fix jdk 8 incompatibility issue

--- core/src/main/java/org/apache/cxf/staxutils/validation/W3CMultiSchemaFactory.java
+++ core/src/main/java/org/apache/cxf/staxutils/validation/W3CMultiSchemaFactory.java
@@ -65,8 +65,10 @@ import org.codehaus.stax2.validation.XMLValidationSchema;
 /**
  * Legacy implementation for Woostox 5.x. For Woodstox 6.2+, use W3CMultiSchemaFactory in
  * Woodstox itself.
+ *
+ * @deprecated scheduled for removal, use W3CMultiSchemaFactory from Woodstox 6.2+ instead
  */
-@Deprecated(forRemoval = true, since = "4.2.1")
+@Deprecated
 public class W3CMultiSchemaFactory {
     private static final Logger LOG = LogUtils.getL7dLogger(W3CMultiSchemaFactory.class);
             
